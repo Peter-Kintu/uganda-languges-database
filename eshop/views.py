f11e53dfd57a627d07045e9a9196188f496b1072
Kintu Peter <kintupeter721@gmail.com>
2025-08-24 18:54:06 +0300

    Applied migrations on Neon and preparing to deploy to Render

--- eshop/views.py
+++ eshop/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from .forms import ProductForm
 from .models import Product
 
@@ -8,22 +9,22 @@ def create_product(request):
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('success_page')  # This will now work correctly
+            return redirect('success_page')
     else:
         form = ProductForm()
-    
+
     return render(request, 'eshop/create_product.html', {'form': form})
 
 # View for listing all products
 def product_list(request):
-    products = Product.objects.all().order_by('-id')  # Show most recent products first
+    products = Product.objects.all().order_by('-id')
     return render(request, 'eshop/product_list.html', {'products': products})
 
 # View for the success page
 def success_page(request):
     return render(request, 'eshop/success.html')
 
-# You'll also need a detail view for individual products
+# View for individual product details
 def product_detail(request, slug):
-    product = Product.objects.get(slug=slug)
+    product = get_object_or_404(Product, slug=slug)
     return render(request, 'eshop/product_detail.html', {'product': product})
