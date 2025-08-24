7e0f9189007c1fe37cc073512031d86d88d28076
Kintu Peter <kintupeter721@gmail.com>
2025-08-24 17:57:37 +0300

    Applied migrations on Neon and preparing to deploy to Render

--- eshop/urls.py
+++ eshop/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('sell/', views.create_product, name='create_product'),
     path('product/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('success/', views.success_page, name='success_page'),
 ]
--- eshop/views.py
+++ eshop/views.py
@@ -8,7 +8,7 @@ def create_product(request):
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('success_page')  # Redirect to a success page or the product list
+            return redirect('success_page')  # This will now work correctly
     else:
         form = ProductForm()
     
@@ -18,6 +18,10 @@ def create_product(request):
 def product_list(request):
     products = Product.objects.all().order_by('-id')  # Show most recent products first
     return render(request, 'eshop/product_list.html', {'products': products})
+
+# View for the success page
+def success_page(request):
+    return render(request, 'eshop/success.html')
 
 # You'll also need a detail view for individual products
 def product_detail(request, slug):
