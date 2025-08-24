8aa28731b2af3c57201c538540e65770038b8ac7
Kintu Peter <kintupeter721@gmail.com>
2025-08-24 22:35:33 +0300

    Applied migrations on Neon and preparing to deploy to Render

--- eshop/admin.py
+++ eshop/admin.py
@@ -1,16 +1,16 @@
-# from django.contrib import admin
-# from .models import Product
+from django.contrib import admin
+from .models import Product
 
-# @admin.register(Product)
-# class ProductAdmin(admin.ModelAdmin):
-#     list_display = (
-#         'name',
-#         'price',
-#         'is_negotiable',
-#         'language_tag',
-#         'whatsapp_number',
-#         'slug',
-#     )
-#     list_filter = ('language_tag', 'is_negotiable')
-#     search_fields = ('name', 'description', 'language_tag', 'whatsapp_number')
-#     prepopulated_fields = {'slug': ('name',)}
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'price',
+        'is_negotiable',
+        'language_tag',
+        'whatsapp_number',
+        'slug',
+    )
+    list_filter = ('language_tag', 'is_negotiable')
+    search_fields = ('name', 'description', 'language_tag', 'whatsapp_number')
+    prepopulated_fields = {'slug': ('name',)}
