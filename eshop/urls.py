c5693bf9783cf79e0c06bce39f9d1a07fd2c560f
Kintu Peter <kintupeter721@gmail.com>
2025-08-24 19:41:14 +0300

    Applied migrations on Neon and preparing to deploy to Render

--- eshop/urls.py
+++ eshop/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'eshop'  #  Enables namespaced URL resolution
+
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('sell/', views.create_product, name='create_product'),
