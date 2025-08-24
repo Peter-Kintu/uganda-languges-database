01b92ff93bad137bfaf499e7167934f50bae2365
Kintu Peter <kintupeter721@gmail.com>
2025-08-24 21:22:23 +0300

    Applied migrations on Neon and preparing to deploy to Render

--- eshop/admin.py
+++ eshop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'price',
+        'is_negotiable',
+        'language_tag',
+        'whatsapp_number',
+        'slug',
+    )
+    list_filter = ('language_tag', 'is_negotiable')
+    search_fields = ('name', 'description', 'language_tag', 'whatsapp_number')
+    prepopulated_fields = {'slug': ('name',)}
--- eshop/urls.py
+++ eshop/urls.py
@@ -5,7 +5,5 @@ app_name = 'eshop'  #  Enables namespaced URL resolution
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
-    path('sell/', views.create_product, name='create_product'),
     path('product/<slug:slug>/', views.product_detail, name='product_detail'),
-    path('success/', views.success_page, name='success_page'),
 ]
