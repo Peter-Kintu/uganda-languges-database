ec21aacd8ce68106fac67c23348ca806cfd6681c
Kintu Peter <kintupeter721@gmail.com>
2025-08-27 03:41:20 +0300

    Applied migrations on Neon and preparing to deploy to Render

--- eshop/urls.py
+++ eshop/urls.py
@@ -5,5 +5,8 @@ app_name = 'eshop'
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
+    path('add/', views.add_product, name='add_product'),
     path('product/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'), # NEW
+    path('cart/', views.view_cart, name='view_cart'), # NEW
 ]
