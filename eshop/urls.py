795cab4677df6e84f79e39efcd988a5880e7f2df
Kintu Peter <kintupeter721@gmail.com>
2025-11-16 16:46:29 +0300

    Updated login page with background slideshow

--- eshop/urls.py
+++ eshop/urls.py
@@ -7,9 +7,8 @@ urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('add/', views.add_product, name='add_product'),
     path('product/<slug:slug>/', views.product_detail, name='product_detail'),
-    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'), # NEW
-    path('cart/', views.view_cart, name='view_cart'), # NEW
+    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/', views.view_cart, name='view_cart'),
     path('checkout/', views.checkout_view, name='checkout'),
-     path('cart/remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('cart/remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
-
