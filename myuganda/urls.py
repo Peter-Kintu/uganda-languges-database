31ca1e8536fc8a57c329e4ffa1d7c3bedf4898a2
Kintu Peter <kintupeter721@gmail.com>
2025-11-17 23:44:29 +0300

    Updated login page with background slideshow

--- myuganda/urls.py
+++ myuganda/urls.py
@@ -2,13 +2,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import StaticViewSitemap  # Make sure you create this file
+
+sitemaps_dict = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('languages.urls')),
     path('eshop/', include('eshop.urls', namespace='eshop')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps_dict}, name='sitemap'),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
