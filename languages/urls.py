6ff0db1af7539f97111fad5c6cf91a3db504c540
Kintu Peter <kintupeter721@gmail.com>
2025-11-17 23:07:14 +0300

    Updated login page with background slideshow

--- languages/urls.py
+++ languages/urls.py
@@ -5,6 +5,8 @@ from . import views
 from .views import google_verification
 
 
+from .views import robots_txt
+
 
 # Set the app name for namespacing. This is a best practice
 # that helps prevent URL name clashes with other apps.
@@ -12,6 +14,7 @@ app_name = 'languages'
 
 urlpatterns = [
     path('googlec0826a61eabee54e.html', google_verification),
+    path("robots.txt", robots_txt),
     # The URL for the root of your application, pointing to the browse view.
     path('', views.browse_contributions, name='home'),
     
