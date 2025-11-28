03a8c8a443e4623d0093948c999beb920a964baa
Kintu Peter <kintupeter721@gmail.com>
2025-11-28 06:31:25 +0300

    Updated login page with background slideshow

--- myuganda/urls.py
+++ myuganda/urls.py
@@ -5,10 +5,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
-from .sitemaps import StaticViewSitemap  # Make sure you create this file
+from .sitemaps import JobPostSitemap, ProductSitemap, StaticViewSitemap  # Make sure you create this file
 
 sitemaps_dict = {
     'static': StaticViewSitemap,
+    'products': ProductSitemap, # NEW: Dynamic sitemap for Eshop products
+    'jobs': JobPostSitemap,
 }
 
 urlpatterns = [
@@ -20,6 +22,7 @@ urlpatterns = [
     path('', include('languages.urls')),
     path('eshop/', include('eshop.urls', namespace='eshop')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps_dict}, name='sitemap'),
+    
 ]
 
 if settings.DEBUG:
