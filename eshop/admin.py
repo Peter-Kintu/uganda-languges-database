8f70e46f470b42ed1404526da5d8413e0f3303e8
Peter Kintu <kintupeter7@gmail.com>
2026-01-20 21:15:35 +0300

    Updated login page with background slideshow

--- eshop/admin.py
+++ eshop/admin.py
@@ -5,7 +5,7 @@ from .views import export_products_json, sync_aliexpress_products
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    # We are keeping all your fields since you confirmed they were working before
+    # Keeping all original fields as they are verified to work with your model
     list_display = (
         'name',
         'vendor_name',
@@ -26,10 +26,18 @@ class ProductAdmin(admin.ModelAdmin):
     def get_urls(self):
         urls = super().get_urls()
         custom_urls = [
-            # 1. We change the internal path to 'sync-now' to avoid clashing with the storefront
-            # 2. We change the name to 'sync_aliexpress_admin_unique'
-            path('sync-now/', self.admin_site.admin_view(sync_aliexpress_products), name='sync_aliexpress_admin_unique'),
+            # 1. Path changed to 'sync-now/' to avoid clashing with the 'eshop/sync-aliexpress/' storefront URL
+            # 2. Name set to 'sync_aliexpress_admin_unique' to avoid reverse lookup conflicts
+            path(
+                'sync-now/', 
+                self.admin_site.admin_view(sync_aliexpress_products), 
+                name='sync_aliexpress_admin_unique'
+            ),
             
-            path('export-json/', self.admin_site.admin_view(export_products_json), name='eshop_product_export_json'),
+            path(
+                'export-json/', 
+                self.admin_site.admin_view(export_products_json), 
+                name='eshop_product_export_json'
+            ),
         ]
         return custom_urls + urls
