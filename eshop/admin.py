c419f6a37cf370cdb1ac4db6c2bc25f34af208c3
Peter Kintu <kintupeter7@gmail.com>
2026-01-20 20:44:38 +0300

    Updated login page with background slideshow

--- eshop/admin.py
+++ eshop/admin.py
@@ -5,8 +5,8 @@ from .views import export_products_json, sync_aliexpress_products
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    # This template MUST exist in your templates folder to avoid 500 error
-    change_list_template = "admin/eshop/product_change_list.html" 
+    # REMOVED change_list_template to stop the 500 error
+    # We will use Jazzmin Top Menu instead to show the button
     
     list_display = (
         'name',
@@ -22,13 +22,13 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields = ('name', 'description', 'vendor_name', 'country', 'whatsapp_number')
     prepopulated_fields = {'slug': ('name',)}
     
-    # Ensures foreign keys/images load efficiently
     list_select_related = [] 
 
     def get_urls(self):
         urls = super().get_urls()
         custom_urls = [
             path('export-json/', self.admin_site.admin_view(export_products_json), name='eshop_product_export_json'),
-            path('sync-aliexpress-admin-action/', self.admin_site.admin_view(sync_aliexpress_products), name='sync_aliexpress_admin'),
+            # This is the path the Jazzmin button will point to
+            path('sync-aliexpress/', self.admin_site.admin_view(sync_aliexpress_products), name='sync_aliexpress_admin'),
         ]
         return custom_urls + urls
