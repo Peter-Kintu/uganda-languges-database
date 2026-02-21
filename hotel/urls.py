0e830a9cb739cac7be02e2d90fcb59a749c0c55c
Peter Kintu <kintupeter7@gmail.com>
2026-02-21 15:34:19 +0300

    Updated login page with background slideshow

--- hotel/urls.py
+++ hotel/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     # 1. Static/Specific paths first
     path('', views.hotel_list, name='hotel_list'),
     path('add/', views.add_accommodation, name='add_accommodation'),
-    path('sync/', views.sync_hotels_travelpayouts, name='sync_hotels'), # FIXED ORDER
+    # hotel/urls.py
+    path('sync/', views.sync_hotels_travelpayouts, name='sync_hotels_travelpayouts'),
     
     # 2. Dynamic/Variable paths last
     path('hotel/<slug:slug>/', views.hotel_detail, name='hotel_detail'),
